ab3fb086d9aea26730ec4cb08356159210402cee
CHRISTIAN <christian019@binus.ac.id>
2021-05-04 12:21:57 +0000

    Deleted storage

--- storage/framework/views/339db6b71e038fd018d4754a49372975ac24abe2.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <h1><?php echo e($title); ?></h1>
-    <p>The best tukang platform in Indonesia</p>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
