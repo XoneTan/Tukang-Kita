397bf7918dc5e51b2d9bdd4155a748a76e200695
miuze <delvin.nicholas@gmail.com>
2021-05-12 22:53:26 +0700

    Update Profile  + Detail

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -1,42 +1,195 @@
 @extends('layouts.app')
+<style>
+    body {
+        background: #f5f5f5;
+        margin-top: 20px;
+    }
+
+    /*------- portfolio -------*/
+    .project {
+        margin: 15px 0;
+    }
+
+    .no-gutter .project {
+        margin: 0 !important;
+        padding: 0 !important;
+    }
+
+    .has-spacer {
+        margin-left: 30px;
+        margin-right: 30px;
+        margin-bottom: 30px;
+    }
+
+    .has-spacer-extra-space {
+        margin-left: 30px;
+        margin-right: 30px;
+        margin-bottom: 30px;
+    }
+
+    .has-side-spacer {
+        margin-left: 30px;
+        margin-right: 30px;
+    }
+
+    .project-title {
+        font-size: 1.25rem;
+    }
+
+    .project-skill {
+        font-size: 0.9rem;
+        font-weight: 400;
+        letter-spacing: 0.06rem;
+    }
+
+    .project-info-box {
+        margin: 15px 0;
+        background-color: #b3daf2;
+        padding: 30px 40px;
+        border-radius: 5px;
+    }
+
+    .project-info-box p {
+        margin-bottom: 15px;
+        padding-bottom: 15px;
+        border-bottom: 1px solid #d5dadb;
+    }
+
+    .project-info-box p:last-child {
+        margin-bottom: 0;
+        padding-bottom: 0;
+        border-bottom: none;
+    }
+
+    img {
+        width: 100%;
+        max-width: 100%;
+        height: auto;
+        -webkit-backface-visibility: hidden;
+    }
+
+    .rounded {
+        margin-top: 15px !important;
+        border-radius: 3px !important;
+    }
+
+
+    .mr-5 {
+        margin-right: 5px !important;
+    }
+
+    .mb-0 {
+        margin-bottom: 0 !important;
+    }
+
+    .project-info-box p {
+        margin-bottom: 15px;
+        padding-bottom: 15px;
+        border-bottom: 1px solid #d5dadb;
+    }
+
+    h3 {
+        margin-top: 0px !important;
+        font-weight: 900 !important;
+        color: black !important;
+    }
+
+    h4 {
+        margin-top: 0px !important;
+        font-weight: 900 !important;
+        color: black !important;
+    }
+
+    p {
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        font-weight: 300;
+        color: black;
+        font-size: 1em;
+        letter-spacing: 0.03rem;
+        margin-bottom: 10px;
+    }
+
+    b,
+    strong {
+        font-weight: 900 !important;
+    }
+
+    #myCarousel {
+        margin-top: 15px !important;
+    }
+</style>
 @section('content')
 <!-- <h1>{{$title}}</h1>
     <h1>Delvin Page</h1> -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
 <div class="container">
     <div class="row">
         <div class="col-md-6">
-            <img src="https://via.placeholder.com/400x300/FFB6C1/000000" alt="project-image" class="rounded">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="/assets/image/kingsman workshop.jpg" alt="Los Angeles">
+                    </div>
+
+                    <div class="item">
+                        <img src="/assets/image/aloha carwash.jpg" alt="Chicago">
+                    </div>
+
+                    <div class="item">
+                        <img src="/assets/image/pak jiko.jpg" alt="New York">
+                    </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+            <!--<img src="/assets/image/kingsman workshop.jpg" alt="project-image" class="rounded">-->
             <div class="project-info-box">
-                <p><b>Categories:</b> Design, Illustration</p>
-                <p><b>Skills:</b> Illustrator</p>
+                <div class="col-md-11">
+                    <h3>{{$item}}</h3>
+                </div>
+                <div class="col-md-1">
+                    <button type="button" class="btn btn-outline-danger"><i class="fa fa-heart"></i>
+                </div>
+
+                <p class="mb-0">Deskripsi : <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis vulputate purus. Sed semper,
+                    augue id placerat rutrum, quam elit tincidunt magna, et ultrices felis eros ut diam</p>
             </div><!-- / project-info-box -->
         </div><!-- / column -->
 
         <div class="col-md-5">
             <div class="project-info-box mt-0">
-                <h5>PROJECT DETAILS</h5>
-                <p class="mb-0">Vivamus pellentesque, felis in aliquam ullamcorper, lorem tortor porttitor erat,
-                    hendrerit porta nunc tellus eu lectus. Ut vel imperdiet est. Pellentesque condimentum, dui et blandit laoreet, quam nisi tincidunt tortor.</p>
+                <h3>Lokasi</h3>
+                <p class="mb-0">Alamat : Lorem ipsum</p>
+                <div class="embed-responsive embed-responsive-4by3">
+                    <iframe src='https://api.mapbox.com/styles/v1/skyeyaya/ckokk7tvi0tyc18ozce4u6guh.html?fresh=true&title=false&access_token=pk.eyJ1Ijoic2t5ZXlheWEiLCJhIjoiY2tvazBrenc5MGMxaDJybDVybG9nc2xpZSJ9.FdGVvzozDUZs-0YgZwpfJA'></iframe>
+                </div>
+                <p class="mb-0">Kontak : lorem ipsum</p>
             </div><!-- / project-info-box -->
 
+            <p>ULASAN PALING MEMBANTU</p>
             <div class="project-info-box">
-                <p><b>Client:</b> CUPCAKE CO</p>
-                <p><b>Date:</b> 14.02.2020</p>
-                <p><b>Designer:</b> James Doe</p>
-                <p><b>Tools:</b> Illustrator</p>
-                <p class="mb-0"><b>Budget:</b> $500</p>
+                <h4>Budi</h4>
+                <p class="mb-0">Chizuru is the best rental girlfriend</p>
             </div><!-- / project-info-box -->
 
-            <div class="project-info-box mt-0 mb-0">
-                <p class="mb-0">
-                    <span class="fw-bold mr-10 va-middle hide-mobile">Share:</span>
-                    <a href="#x" class="btn btn-xs btn-facebook btn-circle btn-icon mr-5 mb-0"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#x" class="btn btn-xs btn-twitter btn-circle btn-icon mr-5 mb-0"><i class="fab fa-twitter"></i></a>
-                    <a href="#x" class="btn btn-xs btn-pinterest btn-circle btn-icon mr-5 mb-0"><i class="fab fa-pinterest"></i></a>
-                    <a href="#x" class="btn btn-xs btn-linkedin btn-circle btn-icon mr-5 mb-0"><i class="fab fa-linkedin-in"></i></a>
-                </p>
-            </div><!-- / project-info-box -->
+            <div class="col-sm-1 col-sm-offset-9">
+                <a href="#" class="btn btn-danger">Report Iklan</a>
+            </div><!-- Button -->
         </div><!-- / column -->
 
 
@@ -53,3 +206,4 @@
 @endif
 <p>This is Detail Page for {{$item}} with id {{$id}}</p>
 @endsection
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,5 +1,127 @@
 @extends('layouts.app')
+<style>
+    div {
+        margin: 5px;
+    }
+
+    p {
+        font-weight: bold;
+    }
+
+
+    .info {
+        text-align: center;
+        border: none;
+        border-radius: 10px;
+        background-color: #91d2da;
+        color: black;
+        width: 100%;
+        padding: 5px;
+        box-shadow: 3px 3px #888888;
+    }
+
+    .content {
+        border: none;
+        border-radius: 10px;
+        background-color: #91d2da;
+        color: black;
+        font-size: 16px;
+        box-shadow: 3px 3px #888888;
+        margin-left: 30px;
+        position: relative;
+        padding: 10px 10px 20px 15px;
+    }
+
+    .button {
+        margin-top: 30px;
+        margin-left: 395px;
+        font-weight: bold;
+        margin-bottom: 40px;
+    }
+
+    h4 {
+        font-weight: 900 !important;
+        color: black !important;
+    }
+
+    h3 {
+        padding: 5px 0px 5px 10px;
+        font-weight: bold !important;
+        color: black !important;
+    }
+
+    .submit {
+        margin-left: 5px;
+        background-color: #5ac2b9 !important;
+    }
+
+    .submit:hover {
+        background-color: #007399;
+        color: white !important;
+    }
+
+    /* input[type="text"] {
+        border: 3px;
+        width: 400px;
+        background-color: #489fa1;
+        border-radius: 5px;
+        padding: 5px;
+        margin-left: 8px;
+    }
+
+    input[type="password"] {
+        border: 3px;
+       
+        background-color: #489fa1;
+        border-radius: 5px;
+        padding: 5px;
+        margin-left: 8px;
+    } */
+
+    input[type="text"]:placeholder {
+        color: #d3d3d3;
+    }
+
+    input[type="password"]:placeholder {
+        color: #d3d3d3;
+    }
+</style>
 @section('content')
-    <h1>Profile Page</h1>
-    <h1>Sehila Pages</h1>
+<!-- <h1>Profile</h1>
+    <h1>Sehila Pages</h1> -->
+
+<div class="container">
+
+    <div class="profile">
+        <div class="row">
+            <div class="col-md-2">
+                <div class="info">
+                    <h4>Information</h4>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="content">
+                    <h3>Ubah Biodata Diri</h3>
+                    <div class="form-group">
+                        <label for="email">Email address:</label>
+                        <input type="email" class="form-control" id="email" placeholder="abc@binus.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password :</label>
+                        <input type="password" class="form-control" id="password" placeholder="123">
+                    </div>
+                    <div class="form-group">
+                        <label for="location">Lokasi :</label>
+                        <input type="text" class="form-control" id="location" placeholder="your location. ex: Bogor, Jakarta, ...">
+                    </div>
+                    <button type="button" class="btn submit">
+                        SIMPAN
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
